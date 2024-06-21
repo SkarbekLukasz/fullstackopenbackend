4919a9912dc9e508aa5444a0a8f0531a7e3286ff
Luke <nycthbre@gmail.com>
2024-06-21 23:35:07 +0200

    Exercise 3.7 completion (phonebook)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let data = [
   {
@@ -75,6 +77,12 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(204).end();
   }
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
